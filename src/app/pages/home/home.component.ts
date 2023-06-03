269c7fbf6f95b11c0d86f0d38bb8a1faa4749eda
Pappu Mahato <pappumahato561@gmail.com>
2023-06-03 18:33:11 +0530

    card created

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,16 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  public productdetails : any;
 
-  public constructor(){
-    this.fun1();
-  }
+  public getProductDetail(product:any){
+    console.log(product);
+    this.productdetails= product;
 
-  public fun1(){
-    alert('fun1');
   }
+  public products=[
+    {
+    id: 1,
+    Name: 'product1',
+    Price:'100',
+    description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+    imgURL:'assets/images/home/pic.jpg'
+    },
 
-  public fun2(){
-    alert('fun2');
-  }
+
+    {
+      id: 2,
+      Name: 'product2',
+      Price:'200',
+      Description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+      imgURL:'assets/images/home/pic.jpg'
+    },
+
+    {
+        id: 3,
+        Name: 'product3',
+        Price:'300',
+        Description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+        imgURL:'assets/images/home/pic.jpg'
+    },
+
+    {
+          id: 4,
+          Name: 'product4',
+          Price:'400',
+          Description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+          imgURL:'assets/images/home/pic.jpg'
+    },
+
+    {
+      id: 5,
+      Name: 'product5',
+      Price:'500',
+      Description:'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer',
+      imgURL:'assets/images/home/pic.jpg'
+  },
+
+  ]
 }
